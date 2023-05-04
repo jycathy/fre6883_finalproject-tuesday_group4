149d65b06e10685464fc2107d7b8485495e0f066
EC2 Default User <ec2-user@ip-172-31-19-69.ec2.internal>
2023-05-04 19:56:02 +0000

    final_project 05-04-2023

---
+++ Webscraper.cpp
@@ -0,0 +1,292 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <locale>
+#include "Grouping.h"
+#include "curl/curl.h"
+#include "Webscraper.h"
+
+using namespace std;
+
+StockGroups groups;
+
+const char* cIWB3000SymbolFile = "Russell_3000_component_stocks.csv";
+
+void populateSymbolVector(vector<string>& symbols)
+{
+	ifstream fin;
+	fin.open(cIWB3000SymbolFile, ios::in);
+	string line, name, symbol;
+	while (!fin.eof())
+	{
+		//fin >> line;
+		getline(fin, line);
+		stringstream sin(line);
+		getline(sin, symbol, ',');
+		getline(sin, name);
+		symbols.push_back(symbol);
+	}
+}
+
+int write_data(void* ptr, int size, int nmemb, FILE* stream)
+{
+	size_t written;
+	written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+struct MemoryStruct {
+	char* memory;
+	size_t size;
+};
+void* myrealloc(void* ptr, size_t size)
+{
+	if (ptr)
+		return realloc(ptr, size);
+	else
+		return malloc(size);
+}
+int write_data2(void* ptr, size_t size, size_t nmemb, void* data)
+{
+	size_t realsize = size * nmemb;
+	struct MemoryStruct* mem = (struct MemoryStruct*)data;
+	mem->memory = (char*)myrealloc(mem->memory, mem->size + realsize + 1);
+	if (mem->memory) {
+		memcpy(&(mem->memory[mem->size]), ptr, realsize);
+		mem->size += realsize;
+		mem->memory[mem->size] = 0;
+	}
+	return realsize;
+}
+
+void Webscraper::getStockData()
+{
+	vector<string> symbolList;
+	populateSymbolVector(symbolList);
+	
+	ofstream fout;
+    
+
+	// file pointer to create file that store the data  
+	FILE* fp;
+
+	const char resultfilename[FILENAME_MAX] = "Results.txt";
+
+	// declaration of an object CURL 
+	CURL* handle;
+
+	CURLcode result;
+
+	// set up the program environment that libcurl needs 
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	// curl_easy_init() returns a CURL easy handle 
+	handle = curl_easy_init();
+
+	// if everything's all right with the easy handle... 
+	if (handle)
+	{
+		string url_common = "https://eodhistoricaldata.com/api/eod/";
+		string start_date = "2023-03-01";
+		string end_date = "2023-03-31";
+		string api_token = "644dd3c5307e07.87085785";  // You must replace this API token with yours
+		for (int i = 1; i <= 3; i++){
+		vector<string>::iterator itr = groups.getGroup(i).begin();
+		for (;itr!=symbolList.end();itr++)
+		{
+			struct MemoryStruct data;
+			data.memory = NULL;
+			data.size = 0;
+
+			string symbol = *itr;
+			string url_request = url_common + symbol + ".US?" + "from=" + start_date + "&to=" + end_date + "&api_token=" + api_token + "&period=d";
+			curl_easy_setopt(handle, CURLOPT_URL, url_request.c_str());
+
+			//adding a user agent
+			curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0");
+			curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0);
+			fp = fopen(resultfilename, "ab");
+			fprintf(fp, "%s\n", symbol.c_str());
+			curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+			curl_easy_setopt(handle, CURLOPT_WRITEDATA, fp);
+			result = curl_easy_perform(handle);
+			fprintf(fp, "%c", '\n');
+			fclose(fp);
+
+			// check for errors 
+			if (result != CURLE_OK) {
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+				return;
+			}
+
+			curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data2);
+			curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void*)&data);
+
+			// perform, then store the expected code in result
+			result = curl_easy_perform(handle);
+
+			if (result != CURLE_OK)
+			{
+				// if errors have occured, tell us what is wrong with result
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+				return;
+			}
+			
+			stringstream sData;
+			sData.str(data.memory);
+			string sValue, sDate;
+			double dValue = 0;
+			double sum = 0;
+			string line;
+			cout << symbol << endl;
+			while (getline(sData, line)) {
+				size_t found = line.find('-');
+				if (found != std::string::npos)
+				{
+					cout << line << endl;
+					sDate = line.substr(0, line.find_first_of(','));
+					line.erase(line.find_last_of(','));
+					sValue = line.substr(line.find_first_of(',') + 1);
+					dValue = strtod(sValue.c_str(), NULL);
+					cout << sDate << " " << std::fixed << ::setprecision(6) << dValue << endl;
+				}	
+			}
+			
+			free(data.memory);
+			data.size = 0;
+		}
+		}
+
+	}
+	else
+	{
+		fprintf(stderr, "Curl init failed!\n");
+		return ;
+	}
+
+	// cleanup since you've used curl_easy_init  
+	curl_easy_cleanup(handle);
+
+	// release resources acquired by curl_global_init() 
+	curl_global_cleanup();
+
+    
+}
+
+
+
+
+void Webscraper::getIWVData()
+{
+	vector<string> symbolList;
+	populateSymbolVector(symbolList);
+	
+	ofstream fout;
+    
+
+	// file pointer to create file that store the data  
+	FILE* fp;
+
+	const char resultfilename[FILENAME_MAX] = "Results.txt";
+
+	// declaration of an object CURL 
+	CURL* handle;
+
+	CURLcode result;
+
+	// set up the program environment that libcurl needs 
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	// curl_easy_init() returns a CURL easy handle 
+	handle = curl_easy_init();
+
+	// if everything's all right with the easy handle... 
+	if (handle)
+	{
+		string url_common = "https://eodhistoricaldata.com/api/eod/";
+		string start_date = "2023-03-01";
+		string end_date = "2023-03-31";
+		string api_token = "644dd3c5307e07.87085785"; 
+		
+			struct MemoryStruct data;
+			data.memory = NULL;
+			data.size = 0;
+
+			string symbol = "IWV";
+			string url_request = url_common + symbol + ".US?" + "from=" + start_date + "&to=" + end_date + "&api_token=" + api_token + "&period=d";
+			curl_easy_setopt(handle, CURLOPT_URL, url_request.c_str());
+
+			//adding a user agent
+			curl_easy_setopt(handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0");
+			curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0);
+			fp = fopen(resultfilename, "ab");
+			fprintf(fp, "%s\n", symbol.c_str());
+			curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+			curl_easy_setopt(handle, CURLOPT_WRITEDATA, fp);
+			result = curl_easy_perform(handle);
+			fprintf(fp, "%c", '\n');
+			fclose(fp);
+
+			// check for errors 
+			if (result != CURLE_OK) {
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+				return;
+			}
+
+			curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data2);
+			curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void*)&data);
+
+			// perform, then store the expected code in result
+			result = curl_easy_perform(handle);
+
+			if (result != CURLE_OK)
+			{
+				// if errors have occured, tell us what is wrong with result
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
+				return;
+			}
+			
+			stringstream sData;
+			sData.str(data.memory);
+			string sValue, sDate;
+			double dValue = 0;
+			double sum = 0;
+			string line;
+			cout << symbol << endl;
+			while (getline(sData, line)) {
+				size_t found = line.find('-');
+				if (found != std::string::npos)
+				{
+					cout << line << endl;
+					sDate = line.substr(0, line.find_first_of(','));
+					line.erase(line.find_last_of(','));
+					sValue = line.substr(line.find_first_of(',') + 1);
+					dValue = strtod(sValue.c_str(), NULL);
+					cout << sDate << " " << std::fixed << ::setprecision(6) << dValue << endl;
+				}	
+			}
+			
+			free(data.memory);
+			data.size = 0;
+		}
+
+	}
+	else
+	{
+		fprintf(stderr, "Curl init failed!\n");
+		return ;
+	}
+
+	// cleanup since you've used curl_easy_init  
+	curl_easy_cleanup(handle);
+
+	// release resources acquired by curl_global_init() 
+	curl_global_cleanup();
+
+    
+}
---
+++ Webscraper.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include<cmath>
+#include <fstream>
+#include <locale>
+#include "curl/curl.h"
+
+
+using namespace std;
+namespace fre{
+    class Webscraper{
+        private:
+        string startdate;
+        string enddate;
+        
+        public:
+            Webscraper(string startdate_, string enddate_)
+            {
+                startdate = startdate_;
+                enddate = enddate_;
+            }
+            void getStockData();
+            void getIWVData();
+        
+    };
+
+}
